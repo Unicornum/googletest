a05fc5c0c4ac0f84a4626df6d57634263aeaa77e
unicornum.verum <unicornum.verum@gmail.com>
2024-12-29 12:11:24 +0400

    [externals] Файлы с версией библиотеки.

---
+++ googletest/include/gtest/gtest.hpp
@@ -0,0 +1,15 @@
+
+#pragma once
+
+#if BOOST_COMP_MSVC
+#pragma warning(push)
+#pragma warning(disable: 4996)
+#endif
+
+#include <gtest\gtest.h>
+
+#if BOOST_COMP_MSVC
+#pragma warning(pop)
+#endif
+
+#include <gtest\version.hpp>
